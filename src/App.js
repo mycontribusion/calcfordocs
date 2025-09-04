a107228b197371a811efa8403abaf66a61426e39
Ahmad Musa <mycontribusion01@gmail.com>
2025-09-04 20:34:27 +0100

    only shows one calculator

--- src/App.js
+++ src/App.js
@@ -1,37 +1,12 @@
-import AxisInterpreter from "./calculators/AxisInterpreter";
-import BMICalculator from "./calculators/BMICalculator";
-import ECGInterpreter from "./calculators/ECGInterpreter";
-import FluidCorrection from "./calculators/FluidCorrection";
-import GCSCalculator from "./calculators/GCSCalculator";
-import GlucoseConverter from "./calculators/GlucoseConverter";
-import HeartFailureFramingham from "./calculators/HeartFailureFramingham";
-import MapCalculator from "./calculators/MAPCalculator";
-import WeightEstimator from "./calculators/WeightEstimator";
-import HypokalemiaCorrection from "./calculators/HypokalemiaCorrection";
-import EstimatedBloodVolume from "./calculators/EstimatedBloodVolume";
-import PediatricTransfusionCalculator from "./calculators/PediatricTransfusionCalculator";
-import MilestoneAgeEstimator from "./calculators/MilestoneAgeEstimator";
+import CalcForDocs from "./calculators/CalcForDocs";
 
 
-
-function App() {
-  return (
-    <div className="App">
-      <WeightEstimator/>
-      <AxisInterpreter/>
-      <BMICalculator/>
-      <ECGInterpreter/>
-      <GCSCalculator/>
-      <GlucoseConverter/>
-      <HeartFailureFramingham/>
-      <MapCalculator/>
-      <FluidCorrection/>
-      <HypokalemiaCorrection/>
-      <EstimatedBloodVolume/>
-      <PediatricTransfusionCalculator/>
-      <MilestoneAgeEstimator/>
+function App(){
+  return(
+    <div>
+      <CalcForDocs/>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/calculators/CalcForDocs.js
@@ -0,0 +1,94 @@
+import AxisInterpreter from "./AxisInterpreter";
+import BMICalculator from "./BMICalculator";
+import ECGInterpreter from "./ECGInterpreter";
+import FluidCorrection from "./FluidCorrection";
+import GCSCalculator from "./GCSCalculator";
+import GlucoseConverter from "./GlucoseConverter";
+import HeartFailureFramingham from "./HeartFailureFramingham";
+import MapCalculator from "./MAPCalculator";
+import WeightEstimator from "./WeightEstimator";
+import HypokalemiaCorrection from "./HypokalemiaCorrection";
+import EstimatedBloodVolume from "./EstimatedBloodVolume";
+import PediatricTransfusionCalculator from "./PediatricTransfusionCalculator";
+import MilestoneAgeEstimator from "./MilestoneAgeEstimator";
+import { useState } from "react";
+import calcinfo from './calcinfo.json';
+
+
+
+function CalcForDocs() {
+
+    const[calc, setCalc] = useState(null)
+
+    let calcdata = calcinfo
+
+    let calcbuttons = calcdata.map((item) => (
+        <div className="button" key={item.id}>
+            <button value={item.name} onClick={() => displayCalc(item.id)}>{item.name}</button>
+        </div>
+    ))
+
+    const displayCalc = (id) => {
+        setCalc(id)
+    }
+
+    const renderCalc = () => {
+        switch (calc) {
+            case 'a':
+                return <BMICalculator />;
+            case 'b':
+                return <AxisInterpreter />;
+            case 'c':
+                return <ECGInterpreter />;
+            case 'd':
+                return <GlucoseConverter />;
+            case 'e':
+                return <MapCalculator />;
+            case 'f':
+                return <ECGInterpreter />;
+            case 'g':
+                return <FluidCorrection />;
+            case 'h':
+                return <HeartFailureFramingham />;
+            case 'i':
+                return <ECGInterpreter />;
+            default:
+                return (
+                    <div className="p-8 text-center text-gray-500">
+                        <p className="text-xl">Select a calculator to get started.</p>
+                    </div>
+                );}}
+
+
+    /*const displayCalc = (id) => {
+        setCalc(id)
+    }*/
+
+    return(
+        <div>
+            {calcbuttons}
+        <div>{renderCalc()}</div>
+        </div>
+    )
+
+ /* return (
+    <div className="App">
+      <WeightEstimator/>
+      <AxisInterpreter/>
+      <BMICalculator/>
+      <ECGInterpreter/>
+      <GCSCalculator/>
+      <GlucoseConverter/>
+      <HeartFailureFramingham/>
+      <MapCalculator/>
+      <FluidCorrection/>
+      <HypokalemiaCorrection/>
+      <EstimatedBloodVolume/>
+      <PediatricTransfusionCalculator/>
+      <MilestoneAgeEstimator/>
+    </div>
+  );*/
+
+}
+
+export default CalcForDocs;
