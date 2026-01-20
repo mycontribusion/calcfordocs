15ddfa1c8bc17c8cbd6e40f235c661b557c9b3bf
Ahmad Musa <mycontribusion01@gmail.com>
2026-01-20 23:50:09 +0100

    old oofline filek

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // <-- import
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// This enables offline + background updates
+// Register the service worker for offline support
 serviceWorkerRegistration.register();
