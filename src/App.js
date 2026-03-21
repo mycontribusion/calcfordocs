9fcecd92d54b82cc1951b50593fc145a3ac2edad
Ahmad Musa <mycontribusion01@gmail.com>
2026-03-21 03:37:55 +0100

    vercel analytics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import CalcForDocs from "./CalcForDocs";
-
 
 function App() {
   return (
     <div>
       <CalcForDocs />
+      <Analytics />
+      <SpeedInsights />
     </div>
   )
 }
