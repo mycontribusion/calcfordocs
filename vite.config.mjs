d351e10fc78a0c9ee721dadfc8a4f1969c33407b
Ahmad Musa <mycontribusion01@gmail.com>
2026-07-05 08:53:44 +0100

    return offline feauture

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      strategies: 'injectManifest',
+      srcDir: 'src',
+      filename: 'service-worker.js',
+      injectManifest: {
+        injectionPoint: 'self.__WB_MANIFEST',
+      },
+    }),
+  ],
   build: {
     outDir: 'build',
   },
